6361d69193fd556eb8e583d492a4f3297e97b32f
andrew <162043489+AungMoeThwe@users.noreply.github.com>
2024-03-10 03:10:11 +0000

    Applied some information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aung Moe",
+  title: "Software Engineer",
+  email: "andrew.aungmoe@gmail.com",
+  gitHub: "https://github.com/AungMoeThwe",
+  instagram: "https://www.instagram.com/aung._.moe/",
+  linkedIn: "https://www.linkedin.com/in/aung-moe-thwe-3601702a8/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
